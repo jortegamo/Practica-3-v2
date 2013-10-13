6062c0392d29eb2dc345abf593c11cfb55ba9972
jortegamo <j.ortega.morata@gmail.com>
2013-10-13 14:03:04 +0200

    terminados los test del prototipo 7 y realizado conversiones funcionales a los metodos iterate y detect

--- 07-gameboard/spec/GameBoardSpec.js
+++ 07-gameboard/spec/GameBoardSpec.js
@@ -56,3 +56,146 @@
     colisionado con objetos de cierto tipo, no con todos los objetos.
 
 */
+describe ("Clase GameBoardSpec",function(){
+
+	// primero inicializamos las variables necesarias para comenzar el juego. 
+	var canvas, ctx;
+
+    beforeEach(function(){ // antes de cada test se cargan las siguientes variables y se hacen comprobaciones
+		loadFixtures('index.html');
+	
+		canvas = $('#game')[0];
+		expect(canvas).toExist();
+	
+		ctx = canvas.getContext('2d'); // necesario para los métodos draw
+		expect(ctx).toBeDefined();
+		
+		expect (GameBoard).toBeDefined(); // me aseguro de que GameBoard ha sido definido
+		
+		var sprites = {
+	    	ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
+		};
+	
+		SpriteSheet.load (sprites,function(){}); // necesario para crear un objeto PlayerShip, 
+												// puesto que al crearlo lo extraemos del map de SpriteSheet.
+												// siempre se nos creara un PlayerShip con las prop de ship.
+    });
+
+	it("GameBoard.add", function(){
+	
+		var board = new GameBoard();
+		Game = {width: 320, height: 480}; // necesito Game.w y Game.H para que se cree la nave.
+		var miNave = new PlayerShip();
+		board.add (miNave);
+		// Al llamar al metodo add de board, se habrá añadido el objeto al array objects.
+		runs (function(){	 
+			expect (board.objects.length).toBe(1);
+		});
+    });
+    
+    it("GameBoard.remove", function(){
+	
+		var board = new GameBoard();
+		Game = {width: 320, height: 480}; // necesito Game.w y Game.H para que se cree la nave.
+		var miNave = new PlayerShip();
+		board.resetRemoved(); //Hacemos reset del buffer de objetos a eliminar. (en este caso para inicializar). 
+		expect (board.removed.length).toBe (0);
+		board.remove (miNave);
+		// Al llamar al metodo remove de board, se habrá añadido el objeto al array removed.
+		runs (function(){	 
+			expect (board.removed.length).toBe(1);	
+		});
+    });
+    
+    it ("GameBoard.finalizeRemove",function(){
+    	var board = new GameBoard();
+    	Game = {width: 320, height: 480}; // necesito Game.w y Game.H para que se cree la nave.
+    	var miNave = new PlayerShip ();
+    	board.add (miNave);
+    	board.resetRemoved();
+    	board.remove (miNave);
+    	expect (board.removed.length).toBe(board.objects.length);
+    	board.finalizeRemoved();
+    	//Al llamar al metodo finalizeRemoved, se habrá borrado miNabe del buffer Objects.
+    	runs(function(){
+    		expect (board.objects.length).toBe(0);
+    	});
+    });
+    
+    it ("GameBoard.step",function(){
+    	var board = new GameBoard();
+    	var miNave = {
+    		step: function(){},
+    		draw: function(){}
+    	};
+    	board.add(miNave);
+    	spyOn (miNave, "step");
+    	spyOn (board,"finalizeRemoved")
+    	var dt = 1;
+    	board.step(dt);
+    	runs (function(){ // si se cumplen todas estas llamadas, el metodo step de GameBoard funcionará. 
+    		expect (miNave.step).toHaveBeenCalled(); // comprueba que iterate funciona y que llama a step de miNave.
+    		expect (board.removed.length).toBe (0); // se ha llamado a resetRemoved.
+    		expect (board.finalizeRemoved).toHaveBeenCalled();
+    	});
+    });
+    
+    it ("GameBoard.draw",function(){
+    	var board = new GameBoard();
+    	var miNave = {
+    		step: function (){},
+    		draw: function (){}
+    	};
+    	board.add(miNave);
+    	spyOn (miNave,"draw");
+    	board.draw(ctx);
+    	runs(function(){
+    		expect (miNave.draw).toHaveBeenCalled(); // comprueba que iterate funciona y que llama a draw de miNave.
+    		expect (miNave.draw).toHaveBeenCalledWith(ctx);
+    	});
+    });
+    
+    it ("GameBoard.detect",function(){
+    	// en este test nos creamos una función que es la que va a evaluar la detección del objeto. 
+    	var board = new GameBoard();
+    	var miNave = {x: 1, y: 2};
+    	board.add(miNave);
+    	var f = function (){
+    		return this.x === 1; // ese this será el objeto de objects sobre el que hará la llamada a la función f detect.
+    	}
+    	var objDetected = board.detect (f);
+    	runs(function(){
+    		expect (objDetected).toBe(miNave);
+    	});
+    });
+    
+    it ("GameBoard.collide False Collide",function(){
+    	// en este test se prueba la funcionalidad de GameBoard.collide por lo que tambien se prueba el 
+    	// correcto funcionamiento de overlap y de detect.
+    	var board = new GameBoard();
+    	var miNave = {x: 1, y: 1, w: 2, h: 2}; // les damos valores a las coordenadas para que no exista overlap.
+    	var naveEnemiga = {x: 3,y: 1, w: 1, h: 2};	
+    	board.add (miNave);
+    	board.add (naveEnemiga); // es necesario incluir los dos objetos en el mismo tablero de juego.
+    	expect (board.overlap(miNave,naveEnemiga)).toBe (false); // probamos que overlap funciona correctamente
+    	var col = board.collide (miNave);
+    	runs (function(){
+    		expect (col).toBe (false);
+    	});
+    });
+    
+    it ("GameBoard.collide True Collide",function(){
+    	// en este test se prueba la funcionalidad de GameBoard.collide por lo que tambien se prueba el 
+    	// correcto funcionamiento de overlap y de detect.
+    	var board = new GameBoard();
+    	var miNave = {x: 1, y: 1, w: 2, h: 2}; // les damos valores a las coordenadas para que no exista overlap.
+    	var naveEnemiga = {x: 1,y: 1, w: 1, h: 2};	
+    	board.add (miNave);
+    	board.add (naveEnemiga); // es necesario incluir los dos objetos en el mismo tablero de juego.
+    	expect (board.overlap(miNave,naveEnemiga)).toBe (true); // probamos que overlap funciona correctamente
+    	var naveCol = board.collide (miNave);
+    	runs (function(){
+    		expect (naveCol).toBe (naveEnemiga);
+    	});
+    }); 
+});
